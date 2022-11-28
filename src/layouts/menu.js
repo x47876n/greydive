7cd8e84cb4e5804eeabe6e17dc79b52ea155cc7c
Christian Lopez <kriz.47@hotmail.com>
2022-11-28 19:12:45 -0300

    fixes and deploy

--- src/layouts/menu.js
+++ src/layouts/menu.js
@@ -1,23 +1,28 @@
 import '../assets/styles/greydive.css';
 
 const Menu = () => {
-  return;
-  <div>
-    <button
-      class="btn cursor"
-      type="button"
-      onClick={() => (window.location.href = '/greydive/#/viamo/testeador-1')}
-    >
-      Viamo
-    </button>
-    <button
-      class="btn cursor"
-      type="button"
-      onClick={() => (window.location.href = '/greydive/#/nebula/testeador-2')}
-    >
-      Nebula
-    </button>
-  </div>;
+  return window.location.hash ? (
+    ''
+  ) : (
+    <div>
+      <button
+        class="btn cursor"
+        type="button"
+        onClick={() => (window.location.href = '/greydive/#/viamo/testeador-1')}
+      >
+        Viamo
+      </button>
+      <button
+        class="btn cursor"
+        type="button"
+        onClick={() =>
+          (window.location.href = '/greydive/#/nebula/testeador-2')
+        }
+      >
+        Nebula
+      </button>
+    </div>
+  );
 };
 
 export default Menu;
